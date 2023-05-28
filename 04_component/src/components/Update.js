48ed66f332797fb874b4dc2290f1a29544c4b37f
mymooss <minseo3238@gmail.com>
2023-05-28 11:27:20 +0900

    5/28

--- 04_component/src/components/Update.js
+++ 04_component/src/components/Update.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
 const Update = ({ title, body, onUpdate }) => {
+  //useState를 사용하여 현재 상태 초기화
+  //현재 Title을 업데이트 함
+  //현재 내용을 업데이트 함
   const [newTitle, setNewTitle] = useState(title);
   const [newBody, setNewBody] = useState(body);
 
@@ -11,13 +14,19 @@ const Update = ({ title, body, onUpdate }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text"value={newTitle}
-        onChange={(e) => setNewTitle(e.target.value)}
+      {/**Title 업데이트 */}
+      <input 
+      type="text"
+      value={newTitle}
+      onChange={(e) => setNewTitle(e.target.value)}
       /><br/>
+      {/**Body 업데이트 */}
       <textarea
         value={newBody}
         onChange={(e) => setNewBody(e.target.value)}
       ></textarea><br/>
+
+      {/**submit 버튼 렌더링 */}
       <button type="submit">Update</button>
     </form>
   );
